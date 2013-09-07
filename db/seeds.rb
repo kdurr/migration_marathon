816ad206f790bd9a5bce4cec699ece4381026c62
kdurr <kristina.l.durr@gmail.com>
2013-09-07 17:14:44 -0400

    added csv and updated seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'csv'
+
+CSV.read('list_of_books', headers: true)
+
+list_of_books.each do |info|
+  book = Book.where({
+    title: info[:title],
+    author: info[:author],
+    rating: info[:rating]
+    }).first
+
+  if book.nil?
+    book = Book.new(info)
+  else
+    book.assign_attributes = info
+  end
+
+  book.save!
+end
+
